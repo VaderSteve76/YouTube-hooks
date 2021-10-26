b1b5c49dc25a78e8dc6e99295373066dfef66283
VaderSteve76 <alverson.s@yahoo.com>
2021-10-26 17:19:43 -0400

    Searchbar

--- youtube-hooks/src/App.js
+++ youtube-hooks/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  }
 
-export default App;
+  componentDidMount() {
+    this.onTermSubmit('Vs Code');
+  }
+
+  onTermSubmit = async (term) => {
+    const res = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    this.setState({
+      videos: res.data.items,
+      selectedVideo: res.data.items[0]
+    });
+  };
+
+  render() {
+    return (
+      <SearchBar onFormSubmit={this.onTermSubmit} />
+    );
+  }
+
+};
