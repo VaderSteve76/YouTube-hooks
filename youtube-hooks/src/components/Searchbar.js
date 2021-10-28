54e44fce214d4ab31803898be6d203346af852d9
VaderSteve76 <alverson.s@yahoo.com>
2021-10-28 14:27:14 -0400

    Refactored Searchbar to use hooks

--- youtube-hooks/src/components/Searchbar.js
+++ youtube-hooks/src/components/Searchbar.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class SearchBar extends Component {
+const SearchBar = ({ onFormSubmit}) => {
+  const [term, setTerm] = useState('');
 
-  state = {
-    term: ''
-  }
-
-  onInputChange = (e) => {
-    this.setState({ term: e.target.value });
-  }
-
-  onFormSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   }
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input 
-              type="text" 
-              value={this.state.term} 
-              onChange={this.onInputChange} 
-            /> 
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input 
+            type="text" 
+            value={term} 
+            onChange={(e) => setTerm(e.target.value)} 
+          /> 
+        </div>
+      </form>
+    </div>
+  );
+
+};
+
+export default SearchBar;
